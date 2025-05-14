a61b8f53dfa05563553457c102129e78c504ee1f
Raul Mauro <raul.tecinfo@gmail.com>
2025-05-14 17:44:21 -0300

    Include MainActivity.kt compose model theme and type configs. Anothers configs that was necessary to build project

---
+++ app/src/main/java/com/bipa/teste/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.bipa.teste
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppNavigation()
+        }
+    }
+}
